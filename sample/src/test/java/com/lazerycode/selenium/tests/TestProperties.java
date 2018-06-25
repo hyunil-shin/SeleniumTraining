235a4e3e05cd7b60b6efe33d37c3d6784ff165a6
Hyunil Shin <hyunil.shin@nhnent.com>
2018-06-25 14:27:21 +0900

    update

--- sample/src/test/java/com/lazerycode/selenium/tests/TestProperties.java
+++ sample/src/test/java/com/lazerycode/selenium/tests/TestProperties.java
@@ -3,6 +3,12 @@ package com.lazerycode.selenium.tests;
 public class TestProperties {
 
 	public static String test_url = "https://the-internet.herokuapp.com";
+	
+	// Windows
 	public static String chrome_driver_path = "selenium_standalone_binaries//windows//googlechrome//64bit//chromedriver.exe";
 	public static String firefox_binary_path = "E:\\Program Files\\firefox.exe";
+	
+	// Mac
+	//public static String chrome_driver_path = "selenium_standalone_binaries//osx//googlechrome//64bit//chromedriver";
+	//public static String firefox_binary_path = "";
 }
